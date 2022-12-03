64f0491c6f3e991b5d33e3dfa6dcc97392c78443
tomasherrera1910 <tomymza10@gmail.com>
2022-12-03 15:17:03 -0300

    ej 3 en 3 líneas 142 puntos, (con => function 198)

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -1,10 +1,7 @@
 function distributeGifts(packOfGifts, reindeers) {
-    const giftsWeigth = packOfGifts.reduce((weigthAcc, gift) => {
-      return weigthAcc + gift.length
-    },0)
-    const reindeersCarry = reindeers.reduce((weigthCarryAcc, reindeer) => {
-      return weigthCarryAcc + reindeer.length * 2
-  }, 0)
+  const giftsWeigth = packOfGifts.reduce((weigthAcc, gift) => weigthAcc + gift.length, 0)
+  const reindeersCarry = reindeers.reduce((weigthCarryAcc, reindeer) => weigthCarryAcc + reindeer.length * 2, 0)
+  
   return Math.floor(reindeersCarry / giftsWeigth)
   }
 //   console.log(distributeGifts(['music'], ['midudev', 'pheralb', 'codingwithdani', 'carlosble', 'blasco', 'facundocapua', 'madeval', 'memxd']))
