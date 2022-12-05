d0ab300b802f56e573516aca0f801c894abd9631
tomasherrera1910 <tomymza10@gmail.com>
2022-12-04 22:09:49 -0300

    fix: leí sobre el metódo every y realice esta solución que me parece mejor que la anterior

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,20 +1,16 @@
 function fitsInOneBox(boxes) {
-    let isValid = true
-    const boxSize = (box) => box.l + box.w + box.h
-    boxes
-    .sort((a, b) => boxSize(b) - boxSize(a))
-    .forEach((box, i) => {
-      const next = boxes[i+1] || 0
-      if(box.l <= next.l || box.w <= next.w || box.h <= next.h)
-        isValid = false
-    })
-    return isValid    
+  return boxes
+  .sort((a, b) => b.l - a.l)
+  .every((box, i) => {
+    const next = boxes[i+1] || {l:0,w:0,h:0}
+    return (box.l > next.l && box.w > next.w && box.h > next.h)
+  })
 }
-// console.log(fitsInOneBox([
-//     { l: 1, w: 1, h: 1 },
-//     { l: 3, w: 3, h: 3 },
-//     { l: 2, w: 2, h: 2 }
-//   ]))
+console.log(fitsInOneBox([
+    { l: 1, w: 1, h: 1 },
+    { l: 3, w: 3, h: 3 },
+    { l: 2, w: 2, h: 2 }
+  ]))
 console.log(fitsInOneBox([
     { l: 1, w: 1, h: 1 },
     { l: 2, w: 2, h: 2 },
