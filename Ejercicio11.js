2ffb7bc4991a1ac9e75a56fc0d10900df394dca5
tomasherrera1910 <tomymza10@gmail.com>
2022-12-12 15:51:14 -0300

    reto 11

---
+++ Ejercicio11.js
@@ -0,0 +1,17 @@
+function getCompleted(part, total) {
+    const formatTime = (time) =>{
+        const [h, m, s] = time.split(':');
+        return (Number(h) * 3600) + (Number(m) * 60) + Number(s)
+      } 
+    const completed = formatTime(part)
+    const totalTime = formatTime(total)
+    let MCD_min = completed
+    let MCD_max = totalTime
+    while (MCD_max !== 0) {
+        let aux = MCD_max
+        MCD_max = MCD_min % MCD_max
+        MCD_min = aux
+    }
+    
+    return `${completed/MCD_min}/${totalTime/MCD_min}`
+  }
